4466e058fd6f109d58700ab1855ab62965e01aab
mimo360 <mimo360@gmail.com>
2015-07-20 23:42:17 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/lin/mimo360/t_bone/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.lin.mimo360.t_bone;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
